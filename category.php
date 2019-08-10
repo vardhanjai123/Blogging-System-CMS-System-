53475369deecbdfe56681ec40d895cec5d847a87
Jaivardhan <shekharjai07@gmail.com>
2019-08-10 10:14:48 +0530

    Update category.php

--- category.php
+++ category.php
@@ -1,5 +1,5 @@
 <?php include "includes/db.php"; ?>
-    <?php include "includes/header.php"; ?>?
+    <?php include "includes/header.php"; ?>
 //hello asti 
     <!-- Navigation -->
 <?php include "includes/navigation.php"; ?>
